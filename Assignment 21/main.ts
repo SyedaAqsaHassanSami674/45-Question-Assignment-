96678fcb37c23836bd06b80ad0c0e0557dfa0135
Syeda Aqsa Hassan Sami <166224660+SyedaAqsaHassanSami674@users.noreply.github.com>
2024-04-24 14:45:39 +0500

    Add files via upload

---
+++ Assignment 21/main.ts
@@ -0,0 +1,15 @@
+interface itCourse {
+    courseName: string;
+    location:string;
+    onSiteStudents:number;
+
+}
+
+let itCourse = {
+    courseName: "TypeScript and JavaScript",
+    location: "Governor House Sindh",
+    onSiteStudents: 50000
+}
+
+
+console.log (itCourse);
