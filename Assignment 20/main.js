19417ac001da75f22a3b54ba3f8c6c5f0edb18f9
Syeda Aqsa Hassan Sami <166224660+SyedaAqsaHassanSami674@users.noreply.github.com>
2024-04-24 14:45:03 +0500

    Add files via upload

---
+++ Assignment 20/main.js
@@ -0,0 +1,3 @@
+var programmingLanguages = ['Typescript', 'Javascript', 'Python', 'PHP'];
+console.log("List of Prgramming Language");
+programmingLanguages.forEach(function (language) { return console.log(language); });
---
+++ Assignment 20/main.ts
@@ -0,0 +1,3 @@
+let programmingLanguages : string[] = ['Typescript','Javascript', 'Python', 'PHP'];
+console.log ("List of Prgramming Language");
+programmingLanguages. forEach(language => console.log (language))
