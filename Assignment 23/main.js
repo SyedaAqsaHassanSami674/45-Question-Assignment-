ca0cd420857a4d4aaba42cc2ae7a9dcaa38b0f5b
Syeda Aqsa Hassan Sami <166224660+SyedaAqsaHassanSami674@users.noreply.github.com>
2024-04-24 14:46:07 +0500

    Add files via upload

---
+++ Assignment 23/main.js
@@ -0,0 +1,32 @@
+var five = 5;
+var ten = 10;
+//Test 1
+console.log("\nTest 1: five is equal to five");
+console.log(five == 5);
+//Test 2
+console.log("\nTest 2: Ten is equal to Ten");
+console.log(10 == 10);
+//Test 3
+console.log("\nTest 3: five is not equal to ten");
+console.log(five != ten);
+//Test 4
+console.log("\nTest 4: ten is greater than five");
+console.log(ten > 5);
+//Test 5
+console.log("\nTest 5: five is smaller than 10");
+console.log(five < 10);
+//Test 6
+console.log("\nTest 6:  ten is smaller than 5");
+console.log(ten < 5);
+//Test 7
+console.log("Test 7: five is equal to ten");
+console.log(five == 10);
+//Test 8
+console.log("\nTest 8: ten  is not equal to 10");
+console.log(ten != 10);
+//Test 9
+console.log("\nTest 9: five is greater than to 10");
+console.log(five > 10);
+//Test 10
+console.log("\nTest 1: 100 is smaller than 50");
+console.log(100 < 50);
