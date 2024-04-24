6cb84310310f65c4c48a2f666b0cf08895082f6e
Syeda Aqsa Hassan Sami <166224660+SyedaAqsaHassanSami674@users.noreply.github.com>
2024-04-24 14:40:24 +0500

    Add files via upload

---
+++ Assignment 10/main.ts
@@ -0,0 +1,8 @@
+// Question10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does
+
+
+// Aqsa, 2024-04-07
+// 100 days of coding challenge
+
+let myName: string = "Aqsa";
+console.log(`I'm ${myName}, I really enjoy the 100 days of coding challenge.`)
