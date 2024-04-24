a5673143df826d399702be3f13056276f8df10c2
Syeda Aqsa Hassan Sami <166224660+SyedaAqsaHassanSami674@users.noreply.github.com>
2024-04-24 14:25:57 +0500

    Add files via upload

---
+++ Assignment 02/main.js
@@ -0,0 +1,3 @@
+// Question 2: Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var Name = "Syeda Aqsa Hassan Sami";
+console.log("Hello ".concat(Name, ", would you like to learn some Typescript today?"));
